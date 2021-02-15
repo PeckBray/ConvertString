1049d2c5369f69adf5648419d7ae42c7171a5bb2
Brayden Peck <brayden.peck2@gmail.com>
2021-02-14 20:07:51 -0700

    Add project files.

---
+++ ConvertString.vb
@@ -0,0 +1,38 @@
+﻿'Brayden Peck
+'RCET0625
+'Spring 2021
+'Convert String
+'
+Module ConvertString
+
+    Sub Main()
+        Dim userInput1 As String
+        Dim integerReturn As Integer
+        Dim message As String
+
+        userInput1 = Console.ReadLine()
+
+        message = ConvertString(userInput1, integerReturn)
+        Console.WriteLine($"{message}")
+        Console.Read()
+    End Sub
+
+    Function ConvertString(input As String, integerInput As Integer)
+        Dim message As String
+        Try
+            integerInput = CInt(input)
+        Catch ex As Exception
+            If input = "" Then
+                message = ("There was no input")
+            Else
+                message = ("input must contain a number")
+            End If
+        End Try
+
+        Return (message)
+
+    End Function
+
+
+
+End Module
